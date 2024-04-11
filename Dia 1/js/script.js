d856a404123e7e603c8f2910e2d94b988e355060
Andres <andresitopedraza27@gmail.com>
2024-04-11 08:59:10 -0500

    feat: :rocket: Estructura
    Se agrego mas datos de esctructura de javascript

--- Dia 1/js/script.js
+++ Dia 1/js/script.js
@@ -19,3 +19,43 @@ operacion= (8*5)
 console.log(concatenacion)
 
 console.log(typeof c)
+
+
+// ** FUNCION SIN RETORNO Y SIN PARAMETROS **
+function funcionNormal(){
+    console.log("Mi función");
+}
+
+// ** FUNCION SIN RETORNO Y CON PARAMETROS **
+function suma(a,b){
+    console.log(a+b);
+}
+// ** FUNCION CON RETORNO Y CON PARAMETROS **
+function sumaR(a,b){
+    //console.log(a+b);
+    return a+b;
+}
+// ** FUNCION CON RETORNO Y SIN PARAMETROS **
+function salonFavorito(){
+    //console.log(a+b);
+    return "P1";
+}
+
+// ** BUCLE FOR **
+
+for (let i=0;i<10;i=i+1) {
+    console.log(i);
+}
+
+
+
+
+
+
+// ** EJERCICIO 1 **
+
+function celsiusAfahrenheit(celsius) {
+    var fahrenheit = (celsius*9/5)+32;
+    console.log(fahrenheit);
+  }
+celsiusAfahrenheit(10)
