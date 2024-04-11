389a0ad33940285dd68aa16ca707ce28b9d3b001
Andres <andresitopedraza27@gmail.com>
2024-04-11 08:01:00 -0500

    feat: :tada: Inicializacion js
    Inicializacion a fundamentos de javascript

---
+++ Dia 1/js/script.js
@@ -0,0 +1,21 @@
+console.log("Hola mundo")
+
+let a=1;
+let b=5;
+
+console.log(typeof a)
+
+let c=1;
+let d=5;
+
+palabra1= "campus"
+palabra2= "Land";
+
+concatenacion= palabra1+palabra2;
+console.log(concatenacion)
+
+operacion= (8*5)
+
+console.log(concatenacion)
+
+console.log(typeof c)
